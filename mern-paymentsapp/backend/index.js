3d25ff8aeab329e697169493d7884d5b2404babf
exploreIndia <exploreindia830@gmail.com>
2024-06-19 01:57:43 +0530

    Update index.js

--- mern-paymentsapp/backend/index.js
+++ mern-paymentsapp/backend/index.js
@@ -4,15 +4,16 @@ const cors = require("cors");
 const helmet = require('helmet');
 app.use(express.json());
 app.use(helmet());
+app.use((req, res, next) => {
+   res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' blob:;");
+   next();
+ });
 app.use(cors({
    origin:["https://mern-paymentapp.vercel.app"],
    methods:["POST","GET"],
    credentials:true
 }));
-app.use((req, res, next) => {
-   res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' blob:;");
-   next();
- });
+
 app.get("/",(req,res)=>{
    res.json({msg:"Welcome to my API"});
 })
