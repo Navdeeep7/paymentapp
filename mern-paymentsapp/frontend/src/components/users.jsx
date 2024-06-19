b236fda5c9c0d0c2974506259ef52028adf6c053
exploreIndia <exploreindia830@gmail.com>
2024-06-19 12:38:32 +0530

    user spinner

--- mern-paymentsapp/frontend/src/components/users.jsx
+++ mern-paymentsapp/frontend/src/components/users.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
+import PropagateLoader from "react-spinners/PropagateLoader";
 export function Users() {
   const [users, setUsers] = useState([]);
   const [filter, setFilter] = useState('');
-
+  const [loading,setLoading]=useState(false);
   useEffect(() => {
     let mounted = true; // Flag to track if component is mounted
     const token = localStorage.getItem("token");
     const fetchData = async () => {
+      setLoading(true);
       try {
         const response = await axios.get(`https://mern-paymentapp.vercel.app/api/v1/user/bulk?filter=${filter}`, {
           headers: {
@@ -20,6 +21,9 @@ export function Users() {
         );
         if (mounted) { // Check if component is still mounted before updating state
           setUsers(response.data.user);
+          if(response.data){
+            setLoading(false);
+           }
           console.log(users);
         }
       } catch (error) {
@@ -53,38 +57,46 @@ export function Users() {
         className="border-2 rounded-lg p-1 w-full"
       />
       <hr className="mt-1 border-gray-100" />
+
+
+      {loading ? (
+        <div className='flex justify-center items-center p-40'><PropagateLoader color="#4f4f4f" /></div>
+      ) : (
+        <div>
+        <div className='flex flex-col '>
+        {users.map(function(user){ 
+          return(<div key={user._id}  className=''>
+            <div className="flex justify-between mt-2 border p-2 rounded-lg ">
+            <div className="flex items-center">
+              <div className="bg-gray-500 w-8 h-8 flex justify-center items-center text-white font-semibold rounded-full">
+                {user.firstName.charAt(0).toUpperCase()}
+              </div>
+              <div className="font-semibold ml-2">
+                {user.firstName} {user.lastName}
+              </div>
+            </div>
+            <div>
+              <button
+                type="button" onClick={(e)=>{
+                  navigate("/sendmoney?id="+user._id+"&name="+user.firstName);
+  
+                }}
+                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+              >
+                Send Money
+              </button>
+            </div>
+            
+          </div>
+           </div>
+  
+            
+          )
+        })}
+        </div></div>
+      )}
+
       
-      <div className='flex flex-col '>
-      {users.map(function(user){ 
-        return(<div key={user._id}  className=''>
-          <div className="flex justify-between mt-2 border p-2 rounded-lg ">
-          <div className="flex items-center">
-            <div className="bg-gray-500 w-8 h-8 flex justify-center items-center text-white font-semibold rounded-full">
-              {user.firstName.charAt(0).toUpperCase()}
-            </div>
-            <div className="font-semibold ml-2">
-              {user.firstName} {user.lastName}
-            </div>
-          </div>
-          <div>
-            <button
-              type="button" onClick={(e)=>{
-                navigate("/sendmoney?id="+user._id+"&name="+user.firstName);
-
-              }}
-              className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-            >
-              Send Money
-            </button>
-          </div>
-          
-        </div>
-         </div>
-
-          
-        )
-      })}
-      </div>
     </div>
   );
 }
