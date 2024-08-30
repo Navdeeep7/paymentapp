3bffe0606d8ed5c51a909d44129ef528619078ce
Navdeep <navis12.ns@gmail.com>
2024-08-31 00:30:28 +0530

    Update inputbox.jsx

--- mern-paymentsapp/frontend/src/components/inputbox.jsx
+++ mern-paymentsapp/frontend/src/components/inputbox.jsx
@@ -1,10 +1,10 @@
-export function InputBox({ label, placeholder, onChange }){
+export function InputBox({ label, placeholder, onChange ,value}){
     return (
         
         <div className="mt-2 text-left">
         <label htmlFor="fname" className="font-semibold text-sm">{label}</label><br />
         <input 
-          type="text"  onChange={onChange}
+          type="text"  onChange={onChange} value={value}
           placeholder={placeholder} 
           className="w-full text-base p-1 border border-gray-300 rounded mt-1 focus:outline-none focus:border-blue-500"
         />
